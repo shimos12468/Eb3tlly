329b7a727a370e056856447b0950948e1d2961d2
Shreef <armjldtrainer@gmail.com>
2020-06-16 19:41:41 +0200

    Adding the FB Page + Disabled the firebase crashyltics

--- app/src/main/java/com/armjld/eb3tly/About.java
+++ app/src/main/java/com/armjld/eb3tly/About.java
@@ -5,23 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
     String version;
     TextView txtVersion;
+    ImageView imgFacebook;
+    String fbLink = "https://www.facebook.com/Eb3tlyy/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         txtVersion = findViewById(R.id.txtVersion);
+        imgFacebook = findViewById(R.id.imgFacebook);
 
         //Title Bar
         TextView tbTitle = findViewById(R.id.toolbar_title);
         tbTitle.setText("عن البرنامج");
+
+        imgFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebURL(fbLink);
+            }
+        });
 
         try {
             PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
@@ -30,5 +43,10 @@ public class About extends AppCompatActivity {
             e.printStackTrace();
         }
         txtVersion.setText("Current Version : V " + version);
+    }
+
+    public void openWebURL(String inURL) {
+        Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse(inURL) );
+        startActivity(browse);
     }
 }
