e4107c5d718a83ea0edce151f3ad69e4381b049e
Shreef <armjldtrainer@gmail.com>
2020-06-18 23:04:24 +0200

    How to For supplier is up and running

--- app/src/main/java/com/armjld/eb3tly/Notifications.java
+++ app/src/main/java/com/armjld/eb3tly/Notifications.java
@@ -168,14 +168,16 @@ public class Notifications extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()) {
                     for (DataSnapshot ds : snapshot.getChildren()) {
-                        int noticount = (int) ds.getChildrenCount();
-                        Log.i(TAG, "The user has : " + noticount + " Notfications");
-                        notiData notiDB = ds.getValue(notiData.class);
-                        mm[(int) count] = notiDB;
-                        count++;
-                        NotiAdaptere orderAdapter = new NotiAdaptere(Notifications.this, mm, getApplicationContext(), count, mSwipeRefreshLayout);
-                        nDatabase.child(mAuth.getCurrentUser().getUid()).child(Objects.requireNonNull(ds.getKey())).child("isRead").setValue("true");
-                        recyclerView.setAdapter(orderAdapter);
+                        if(ds.exists()) {
+                            int noticount = (int) ds.getChildrenCount();
+                            Log.i(TAG, "The user has : " + noticount + " Notfications");
+                            notiData notiDB = ds.getValue(notiData.class);
+                            mm[(int) count] = notiDB;
+                            count++;
+                            NotiAdaptere orderAdapter = new NotiAdaptere(Notifications.this, mm, getApplicationContext(), count, mSwipeRefreshLayout);
+                            nDatabase.child(mAuth.getCurrentUser().getUid()).child(Objects.requireNonNull(ds.getKey())).child("isRead").setValue("true");
+                            recyclerView.setAdapter(orderAdapter);
+                        }
                     }
                 } else {
                     Log.i(TAG, "No Notifications for this user");
